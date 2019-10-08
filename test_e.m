c45ad0c84f27dca60c9af368eb9ae05a9b643853
Marios S. Kyriakou <mariosmsk@gmail.com>
2019-10-08 17:23:22 +0300

    Delete test_e.m

--- test_e.m
+++
@@ -1,7 +0,0 @@
-inpname='bwcn8-168-true-ec.inp';
-d=epanet(inpname);
-% delete controls and add them back
-controls = d.getControls;
-d.deleteControls;
-d.addControls(controls)
-controls1 = d.getControls;
