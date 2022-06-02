c7d84e2392281525e4c96ae54f2c5b198fa24aff
Marios S. Kyriakou <mkiria01@ucy.ac.cy>
2022-06-02 17:44:32 +0300

    Update epanet.m

--- epanet_matlab_toolkit/epanet.m
+++ epanet_matlab_toolkit/epanet.m
@@ -388,7 +388,7 @@ classdef epanet <handle
         CMDCODE;                     % Code=1 Hide, Code=0 Show (messages at command window)
     end
     properties (Constant = true)
-        classversion='v2.2.002'; % 02/05/2022
+        classversion='v2.2.003'; % 02/06/2022
 
         LOGOP={'IF', 'AND', 'OR'} % Constants for rule-based controls: 'IF', 'AND', 'OR' % EPANET Version 2.2
         RULEOBJECT={'NODE', 'LINK', 'SYSTEM'}; % Constants for rule-based controls: 'NODE', 'LINK', 'SYSTEM' % EPANET Version 2.2
@@ -7902,14 +7902,18 @@ classdef epanet <handle
         function value = getComputedTimeSeries(obj)
             obj.saveInputFile(obj.TempInpFile);
             [fid, binfile, ~] = runEPANETexe(obj);
-            value = readEpanetBin(fid, binfile, 0);
-            value.StatusStr = obj.TYPEBINSTATUS(value.Status + 1);
-            % Remove report bin, files @#
-            warning off;
-            fclose('all');
-            files=dir('@#*');
-            if ~isempty(files); delete('@#*'); end
-            warning on;
+            if fid < 0
+                value = obj.getComputedTimeSeries_ENepanet();
+            else
+                value = readEpanetBin(fid, binfile, 0);
+                value.StatusStr = obj.TYPEBINSTATUS(value.Status + 1);
+                % Remove report bin, files @#
+                warning off;
+                fclose('all');
+                files=dir('@#*');
+                if ~isempty(files); delete('@#*'); end
+                warning on;
+            end
         end
         function value = getComputedTimeSeries_ENepanet(obj)
             obj.saveInputFile(obj.TempInpFile);
@@ -19359,8 +19363,9 @@ function [fid, binfile, rptfile] = runEPANETexe(obj)
      end
      if isunix
          r = sprintf('%s%s %s %s %s', obj.LibEPANETpath, obj.LibEPANET, obj.BinTempfile, rptfile, binfile);
-     elseif ismac
-         r = sprintf('%s%s %s %s %s', obj.LibEPANETpath, obj.LibEPANET, obj.BinTempfile, rptfile, binfile);
+         if ismac
+            r = sprintf('%s%s %s %s %s', obj.LibEPANETpath, obj.LibEPANET, obj.BinTempfile, rptfile, binfile);
+         end
      end
      if obj.getCMDCODE, [~, ~]=system(r); else, system(r); end
      fid = fopen(binfile, 'r');
