249002af4a2a595a21614e2814315273236b67c4
MariosDem <66776265+MariosDem@users.noreply.github.com>
2022-06-23 00:49:19 +0300

    Update EX6_load_two_inp_files.m

--- examples/EX6_load_two_inp_files.m
+++ examples/EX6_load_two_inp_files.m
@@ -1,41 +1,20 @@
 %% Loads 2 different networks.
 % This example contains:
-%   Using 2 dll files. Load 2 Input files.
+%   Load 2 Input files.
 %   Load networks.
 %   Disp elevations for the two networks.
 %   Plot networks.
 %   Unload libraries.
-%   Delete files who created.
 
 %%
 % Clear 
 clear; close('all'); clc;
 start_toolkit;
 
-% Using 2 dll files. Load 2 Input files.
-arch = computer('arch');
-pwdepanet = fileparts(which('epanet.m'));
-if strcmpi(arch,'win64')
-    LibEPANETpath = [pwdepanet,'/64bit/'];
-elseif strcmpi(arch,'win32')
-    LibEPANETpath = [pwdepanet,'/32bit/'];
-end        
-temp_lib_folder = [LibEPANETpath,''];
-mkdir(temp_lib_folder)
-epanet2_tmp = [temp_lib_folder,'epanet2tmp.dll'];
-dll_pathLib = [LibEPANETpath, 'epanet2.dll'];
-header_pathLib = [LibEPANETpath, 'epanet2.h'];
-try
-    copyfile(dll_pathLib, epanet2_tmp);
-    copyfile(header_pathLib, [temp_lib_folder,'epanet2tmp.h']);
-catch
-end
-
 % Load networks.
-d1 = epanet('Net1.inp', epanet2_tmp);
-warning('off'); % ignore warnings
+d1 = epanet('Net1.inp');
 d2 = epanet('Net2.inp');
-warning('on');
+
 
 % Disp elevations for the two networks.
 disp('Net1 - Elevations:')
@@ -52,8 +31,3 @@ d2.plot;
 % Unload libraries.
 d1.unload;
 d2.unload;
-
-% Delete files who created.
-delete(epanet2_tmp);
-delete([temp_lib_folder,'epanet2tmp.h']);
-% rmdir(temp_lib_folder);
