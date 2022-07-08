d27e92d64d194d55ee618bcb99cfdb3d14c65491
MariosDem <66776265+MariosDem@users.noreply.github.com>
2022-07-08 18:52:42 +0300

    Update EX25_using_parfor.m

--- examples/EX25_using_parfor.m
+++ examples/EX25_using_parfor.m
@@ -4,6 +4,11 @@
 tic
 start_toolkit;
 
+% try
+%     unloadlibrary('epanet2')
+% catch
+% end
+
 d = epanet('Net1.inp'); 
 
 clear H;clc;
@@ -11,11 +16,19 @@ clear H;clc;
 number_scenarios = 100;
 
 parfor i = 1:number_scenarios
-%   d.loadEPANETFile(d.TempInpFile);
+    
+% Uncomment section for MATLAB R2020 and previous versions.
+%                                        
+%     if isunix
+%         loadlibrary(obj.LibEPANET, [obj.LibEPANETpath, obj.LibEPANET, '.h']);
+%     else
+%         loadlibrary([obj.LibEPANETpath, obj.LibEPANET], [obj.LibEPANETpath, obj.LibEPANET, '.h']);
+%     end
+%     d.loadEPANETFile(d.TempInpFile); 
+    
     % set parameters
     elevations = d.getNodeElevations-d.getNodeElevations*rand(1)*.5;
     d.setNodeElevations(elevations*10);
-    % 
     
     % Computed Hydraulics
     H{i} = d.getComputedHydraulicTimeSeries;
