d69e2e13a5ee9f17f016ceff43af209549e59b08
Marios S. Kyriakou <mkiria01@ucy.ac.cy>
2022-07-10 14:31:26 +0300

    update/test examples

--- tests/readInpPaths.m
+++ tests/readInpPaths.m
@@ -2,27 +2,23 @@ function [InpList] = readInpPaths(DataFolder)
 % This function is based on
 % https://www.mathworks.com/matlabcentral/fileexchange/31343-enlist-all-file-names-in-a-folder-and-it-s-subfolders
 % Author: Thokare Nitin D.
-% Modified: Marios S. Kyriakou (rename of function)
-% example:
+% Modified: Marios S. Kyriakou
+%
+% Example:
 % [InpList] = readInpPaths(pwd)
 
 DirContents=dir(DataFolder);
 InpList=[];
-if(strcmpi(computer,'PCWIN') || strcmpi(computer,'PCWIN64'))
-    NameSeperator='\';
-elseif(strcmpi(computer,'GLNX86') || strcmpi(computer,'GLNXA86'))
-    NameSeperator='/';
-end
 
 for i=1:numel(DirContents)
     if(~(strcmpi(DirContents(i).name,'.') || strcmpi(DirContents(i).name,'..')))
         if(~DirContents(i).isdir)
             extension=DirContents(i).name(end-2:end);
             if(numel(find(strcmpi(extension, 'inp')))~=0)
-                InpList=cat(1,InpList,{[DataFolder,NameSeperator,DirContents(i).name]});
+                InpList=cat(1,InpList,{[DataFolder,'/',DirContents(i).name]});
             end
         else
-            getlist=readInpPaths([DataFolder,NameSeperator,DirContents(i).name]);
+            getlist=readInpPaths([DataFolder,'/',DirContents(i).name]);
             InpList=cat(1,InpList,getlist);
         end
     end
