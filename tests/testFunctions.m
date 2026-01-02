ca38d711bd28e98bd0a6ad23158b27d3f85e798f
Marios S. Kyriakou <mariosmsk@gmail.com>
2026-01-02 13:09:13 +0200

    Update testFunctions.m

--- tests/testFunctions.m
+++ tests/testFunctions.m
@@ -3,11 +3,13 @@
 % correctly.
 % Press F10 for step-by-step execution. You may also use the breakpoints, 
 % indicated with a short dash (-) on the left of each line number.
+% Last test: 02/01/2026
+
 clc; clear; close all; clear class;
 start_toolkit;
 
 % Create EPANET object using the INP file
-inpname='Net1.inp'; 
+inpname='Net1.inp';  
 % Net1 Net2 Net3 BWSN_Network_1 example
 
 % version='epanet2'; % version dev2.1
